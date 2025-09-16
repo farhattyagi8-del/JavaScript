04bfc17e9c250938b9a7433bfb29a5bd5c319f07
farhattyagi8-del <farhattyagi8@gmail.com>
2025-09-16 21:15:10 +0530

    Add files

---
+++ first.js
@@ -0,0 +1,208 @@
+// /  Variable and dta type.
+// Console = "farhattyagi";
+// console.log(Console);
+// Name = "jeonkook";
+// console.log(Name);
+// let age = 46;
+// console.log(age);
+// const Passion = "i am a softwere engineer developer";
+
+// console.log(Passion)
+
+// {
+//     let a = 18;
+//     console.log(a);
+
+// }
+// {
+//     let a = 28;
+//     console.log(a);
+// }
+// age = 28;
+// console.log(age);
+// isFollow = true;
+// console.log(Follow);
+
+// let x = null;
+// console.log(x);
+
+// const student = {
+//     name: "farhat",
+//     age: 18,
+//     cgpa: 80,
+//     ispass: true,
+// };
+// student ["age"] = student["age"] + 10;
+// console.log(student["age"]);
+// student ["name"] =  student["name"] = jeonkook;
+// console.log(student["name"]);
+
+// const product = {
+//     name:"Ball pane",
+//     rating:4,
+//     offer:5,
+//     price:260,
+
+// };
+// console.log(product)
+
+// const profile = {
+//     name: "@shradha khapra",
+//     isfollow: true,
+//     post:195,
+//     follwer:569,
+//     following:4,
+//     bio: "Enterpreneur Apnacollegee / Ex-Microsoft DRDO  To educate someone in the gigh privilege" ,
+// };
+
+// // /// Conditional opetarors .
+// Aartimatic operatrors
+// let a = 5;
+// let b = 6;
+// console.log ("a = ",a, "& b = ", b);
+// console.log("a + b = ", a + b);
+// console.log("a - b =", a - b);
+
+// Unary operators
+// let a = 5;
+// let b = 6;
+// a = a + 1;
+// console.log("a = ", a);
+// console.log("++a = ", ++a);
+// console.log("--a = ", --a);
+// console.log("a++ = ", a++);
+// console.log("a-- = ", a--);
+
+// Assigment operators
+
+// let a = 5;
+// let b = 4;
+
+// a += 4;
+// console.log("a = ", a)
+// a -= 4;
+// console.log("a = ", a)
+// a *= 4;
+// console.log("a = ", a)
+// a /= 4;
+// console.log("a = ", a)
+// a %= 4;
+// console.log("a = ", a)
+// a **= 4;
+// console.log("a = ", a)
+
+// Comparison operators
+
+//  let a = 5;
+//  let b = 4;
+//  console.log("5 !=y",  a);
+//  console.log("a == b", a == b);
+//  console.log("a === b", a === b); // Estricate version  comparision of digits and aslo data type
+
+//  console.log("a > b",  a > b)
+//  console.log("a >= b",  a >= b)
+// console.log("a < b",  a < b)
+// console.log("a <= b",  a <= b)
+
+
+// Logical operators
+
+// let a = 5;
+//  let b = 4;
+
+//  let cond1 = a > b;
+//  let cond2 = a === b;
+//  console.log("cond1 && cond2 =", cond1 && cond2);
+
+// let mode = "dark";
+// let color;
+
+// if (mode === "dark"){
+//     color = "black";
+// }
+
+// if ( mode === "light"){
+//     color = "white";
+// }
+
+// console.log(color);
+
+// let mode = "dark";
+// let color;
+ 
+// if ( mode === "dark-mode"){
+//     color = "black";
+// }else{
+//     color = "red"
+// }
+
+// console.log(color);
+
+// let num = 3;
+
+// if( num%2 === 0){
+//     console.log("even number")
+// }else{
+//     console.log("Odd number")
+// }
+// if (age > 18 ){
+//     console.log("can vote and  also get driving licence")
+// }
+
+// let mode  = "dark";
+// let color;
+
+// if(mode === "drak"){
+//     color = "black";
+// }else if(mode === "black"){
+//     color = "black";
+// }else if(mode === "pink"){
+//     color = "pink";
+// }else{ 
+//     color = "white";
+
+// }
+// console.log(color);
+
+// pratice qution
+
+// alert ("hello, frahat")  
+// one time popup 
+
+//  let name = prompt ("hello, developer");
+//  console.log(name);
+//   temperory type to take input from user
+
+//   let num = prompt ("Enter a number");
+//  if ( num %5 === 0){
+//    console.log(num, "is multiple of 5");
+//  }else{
+//    console.log("num is not multuple");
+// }
+
+
+
+//  partice second 
+
+// let score = 80;
+// let = "grade";
+
+// if(score >= 90 && score <= 100){
+//   grade = "A";
+// }else if( score >= 70 && score<= 89){
+//   grade = "B";
+// } else if( score >= 50 && score <= 60){
+//   grade = "C";
+// }else if (score >= 30 && score <= 40){
+//   grade = "D";
+// }
+// console.log("this is your score according to yuor performantion:", grade );` 
+
+
+///////////// Loops in js.
+
+
+for (let count=1; count<=5; count = count++){
+    console.log("farhat developer");
+    
+}
